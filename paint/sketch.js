c638d5f2b7b6ef2a4573073fd49da396efa18116
exismys <riteshzeronity@gmail.com>
2021-09-24 23:22:23 +0530

    removed localhost from client side socket.connect

--- paint/sketch.js
+++ paint/sketch.js
@@ -2,7 +2,7 @@ let socket;
 function setup() {
     createCanvas(400, 400);
     background(0);
-    socket = io.connect('http://localhost:3000')
+    socket = io.connect()
     socket.on("mousePos", (data) => {
         noStroke();
         fill(255, 0, 0);
