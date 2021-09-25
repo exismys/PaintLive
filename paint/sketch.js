a0928bd3452d72504964a21d50557a5fa81b3914
exismys <riteshzeronity@gmail.com>
2021-09-25 17:31:19 +0530

    updated further

--- paint/sketch.js
+++ paint/sketch.js
@@ -5,14 +5,14 @@ function setup() {
     socket = io.connect()
     socket.on("mousePos", (data) => {
         noStroke();
-        fill(random(255), random(255), random(255), 100);
+        fill(255, 0, 0, 100);
         circle(data.x, data.y, 15);
     })
 }
 
 function draw() {
     noStroke();
-    fill(random(255), random(255), random(255), 100);
+    fill(255, 255, 255, 100);
     circle(mouseX, mouseY, 15);
     let data = {
         x: mouseX,
