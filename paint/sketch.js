d53e2412229d521c3c5479f68e1339e362767510
exismys <riteshzeronity@gmail.com>
2021-12-04 21:37:20 +0530

    updated ui

--- paint/sketch.js
+++ paint/sketch.js
@@ -1,22 +1,24 @@
 let socket;
 function setup() {
-    createCanvas(400, 400);
-    background(0);
-    socket = io.connect()
-    socket.on("mousePos", (data) => {
-        noStroke();
-        fill(255, 0, 0, 100);
-        circle(data.x, data.y, 15);
-    })
+  createCanvas(windowWidth * 0.8, windowHeight * 0.8);
+  background(0);
+  socket = io.connect();
+  socket.on("mousePos", (data) => {
+    noStroke();
+    fill(255, 0, 0, 100);
+    circle(data.x, data.y, 15);
+  });
 }
 
-function draw() {
-    noStroke();
-    fill(255, 255, 255, 100);
-    circle(mouseX, mouseY, 15);
-    let data = {
-        x: mouseX,
-        y: mouseY
-    }
-    socket.emit("mousePos", data);
+function draw() {}
+
+function mouseDragged() {
+  noStroke();
+  fill(255, 255, 255, 100);
+  circle(mouseX, mouseY, 15);
+  let data = {
+    x: mouseX,
+    y: mouseY,
+  };
+  socket.emit("mousePos", data);
 }
